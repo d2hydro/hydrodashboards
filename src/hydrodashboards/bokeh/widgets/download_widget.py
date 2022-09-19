e9c21b89f9174fe4fef6759b5205af0b06af59de
Daniel <daniel@d2hydro.nl>
2022-09-19 12:06:14 +0200

    datetime to local time
    closes #1

--- src/hydrodashboards/bokeh/widgets/download_widget.py
+++ src/hydrodashboards/bokeh/widgets/download_widget.py
@@ -56,12 +56,14 @@ if (navigator.msSaveBlob) {
 
 download_js = """
 var data = [
-    [1, 2, 3],
-    ["1", "2", "3"],
-    [0.1, 0.2, 0.3],
-    ["0.1", "0.2", "0.3"],
-    ['', null, undefined],
-    [new Date(2021, 8, 1), new Date(2021, 8, 2), new Date(2021, 8, 3)],
+    ["datum-tijd", "waarde"],
+    [new Date(2021, 8, 1, 1, 0, 0), 0.15],
+    [new Date(2021, 8, 1, 1, 15, 0), 0.20]
+    [new Date(2021, 8, 1, 1, 30, 0), 0.18],
+    [new Date(2021, 8, 1, 1, 45, 0), 0.25],
+    [new Date(2021, 8, 1, 2, 0, 0), 0.3],
+    [new Date(2021, 8, 1, 2, 15, 0), 0.2],
+    [new Date(2021, 8, 1, 2, 30, 0), 0.15]
 ];
 saveToXlsx(data)
 """
