b6955a3d8488bad2b7c427d58a3a17eb2ba47eb8
ngoorden <49673619+ngoorden@users.noreply.github.com>
2022-11-08 16:46:13 +0100

    Update download_widget.py

--- src/hydrodashboards/bokeh/widgets/download_widget.py
+++ src/hydrodashboards/bokeh/widgets/download_widget.py
@@ -161,22 +161,29 @@ sequentialStart(); //
 """
 
 scale_figs_js = """
+console.log(graph_count)
 console.log(button.disabled)
 if (!button.disabled) {
     var fig_len = figure.children[0].children.length;
+    console.log(fig_len)
     const vh = Math.round(parent.innerHeight * 0.6);
-    
-    if (fig_len == 1) {
-            figure.children[0].height = vh;
-            figure.children[0].children[0].height = vh;
-            }
-    else if (fig_len > 1) {
-        figure.children[0].height = Math.round(fig_len * vh / 3);
-        for (let i = 0; i < figure.children[0].children.length; i++) {
-               figure.children[0].children[i].height = Math.round(vh / 3); 
-               }
-               }
-        }
+
+    if (fig_len < graph_count){
+            console.log("fig_len<");
+            figure.children[0].height = Math.round(fig_len * vh / fig_len);
+            for (let i = 0; i < figure.children[0].children.length; i++) {
+                   figure.children[0].children[i].height = Math.round(vh / fig_len); 
+                   }
+                   }
+    else  {
+            console.log("fig_len>");
+            figure.children[0].height = Math.round(fig_len * vh / graph_count); 
+            for (let i = 0; i < figure.children[0].children.length; i++) {
+                   figure.children[0].children[i].height = Math.round(vh / graph_count); 
+                   }
+                   }
+    }
+
    
 """
 
@@ -196,7 +203,7 @@ def make_button(time_figure_layout):
     button.js_on_change(
         "disabled",
         CustomJS(
-            args=dict(button=button, figure=time_figure_layout), code=scale_figs_js
+            args=dict(button=button, figure=time_figure_layout, graph_count=7), code=scale_figs_js
         ),
     )
 
