47da11a89eb38b48001573f66e06f39df109b373
Daniel Tollenaar <daniel@d2hydro.nl>
2024-11-25 18:05:21 +0100

    Update data.py

--- hydrodashboards/bokeh/data.py
+++ hydrodashboards/bokeh/data.py
@@ -589,14 +589,14 @@ class Data:
     def update_time_series_from_cache(self, indices):
         """Update the time_series_sets.time_series from cache if existing."""
         for index in indices:
-            self.logger.info("trying to update from cache")
+            # self.logger.info("trying to update from cache")
             if self.time_series_sets.exists(index):
                 self.time_series_sets.remove(index)
-            self.logger.info(f"cache file found for {index[0]}, {index[1]}")
+            # self.logger.info(f"cache file found for {index[0]}, {index[1]}")
             self.time_series_sets.append_from_cache(
                 *index, self.periods.search_start, self.periods.search_end
             )
-            self.logger.info(f"new cache length: {len(self.time_series_sets)}")
+            # self.logger.info(f"new cache length: {len(self.time_series_sets)}")
 
     def clean_time_series_within_period(self, period, selection="search"):
         self.time_series_sets.time_series = [
@@ -622,7 +622,7 @@ class Data:
 
         # we try to append new time-series from FEWS
         if fews_indices:
-            self.logger.info(f"indices in fews requrest {fews_indices}")
+            self.logger.info(f"fews request for {fews_indices}")
             fews_ts_set = self._get_fews_ts(
                 indices=fews_indices, request_type="headers"
             )
